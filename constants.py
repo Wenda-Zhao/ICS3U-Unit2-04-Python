2fd730de7645b36cff8244c3941e9b62842f9bfc
Ubuntu <ubuntu@ip-172-31-84-246.ec2.internal>
2020-11-25 17:34:58 +0000

    added in pizza.py

---
+++ constants.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Created by Wenda Zhao
+# Created on Nov 2020
+# This program calculates the circumference of a circle
+#     with user input
+
+
+LABOR = 0.75
+RENT = 1.00
+COST_PER_INCH = 0.50
+HST = 0.13
